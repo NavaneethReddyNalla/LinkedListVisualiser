2c9dc9178d97c7cb9f85982bcba32b3ef1740fa8
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-19 01:02:33 +0530

    Fixed small errors * Changed self.title = title to self.title(title) etc in ui/App.py

--- ui/App.py
+++ ui/App.py
@@ -4,8 +4,8 @@ import tkinter as tk
 class App(tk.Tk):
     def __init__(self, title: str = "App", minsize: tuple = (500, 500)):
         super().__init__()
-        self.title = title
-        self.minsize = minsize
+        self.title(title)
+        self.minsize(*minsize)
 
     def run(self):
         self.mainloop()
