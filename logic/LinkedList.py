7d177d250abab68e41809d3e4a7051ff42bd6f70
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-21 14:19:28 +0530

    Fixed the double insertion bug in the linked list

--- logic/LinkedList.py
+++ logic/LinkedList.py
@@ -8,8 +8,8 @@ class LinkedList:
     def insert_begin(self, data: int) -> None:
         if self.head is None:
             self.head = Node(data)
-
-        new_node: Node = Node(data)
-        new_node.next = self.head
-        self.head.prev = new_node
-        self.head = new_node
+        else:
+            new_node: Node = Node(data)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
