20419401d12242c1f01b3a06d45a2e141b6e59b7
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-21 13:56:33 +0530

    Numbers are being displayed now

--- ui/screens/util/draw_ll.py
+++ ui/screens/util/draw_ll.py
@@ -5,6 +5,7 @@ def draw_linked_list(canvas, linked_list):
     while current is not None:
         rect_coords = (coords[0], coords[1], coords[0] + 50, coords[1] + 50)
         canvas.create_rectangle(*rect_coords, fill="black")
+        canvas.create_text(coords[0] + 25, coords[1] + 25, text=f"{current.data}", fill="white")
 
         coords[0] += 100
         current = current.next
