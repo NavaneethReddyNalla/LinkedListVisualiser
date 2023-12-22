3af2f2b95e16af01918de359d9f7cd995eb84c8d
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-22 13:41:58 +0530

    Added ids to the node class

--- logic/Node.py
+++ logic/Node.py
@@ -1,5 +1,8 @@
 class Node:
     def __init__(self, data: int) -> None:
         self.data: int = data
-        self.next: Node = None
-        self.prev: Node = None
+        self.next: Node | None = None
+        self.prev: Node | None = None
+        self.id: int | None = None
+        self.data_id: int | None = None
+        self.arrow_id: int | None = None
