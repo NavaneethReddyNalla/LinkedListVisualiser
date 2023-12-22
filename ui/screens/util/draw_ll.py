b10f7c2b71bfc95c7df7f16791b94def69d84687
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-23 00:57:44 +0530

    Commented out unused code in draw_ll.py

--- ui/screens/util/draw_ll.py
+++ ui/screens/util/draw_ll.py
@@ -5,14 +5,15 @@ def draw_node(canvas, node):
     node.data_id = canvas.create_text(coords[0] + 25, coords[1] + 25, text=f"{node.data}", fill="white")
 
 
-def draw_linked_list(canvas, linked_list):
-    current = linked_list.head
-    coords = [50, 150]
-
-    while current is not None:
-        rect_coords = (coords[0], coords[1], coords[0] + 50, coords[1] + 50)
-        canvas.create_rectangle(*rect_coords, fill="black")
-        canvas.create_text(coords[0] + 25, coords[1] + 25, text=f"{current.data}", fill="white")
-
-        coords[0] += 100
-        current = current.next
+# This function became useless after completing the animation with only one node
+# def draw_linked_list(canvas, linked_list):
+#     current = linked_list.head
+#     coords = [50, 150]
+#
+#     while current is not None:
+#         rect_coords = (coords[0], coords[1], coords[0] + 50, coords[1] + 50)
+#         canvas.create_rectangle(*rect_coords, fill="black")
+#         canvas.create_text(coords[0] + 25, coords[1] + 25, text=f"{current.data}", fill="white")
+#
+#         coords[0] += 100
+#         current = current.next
