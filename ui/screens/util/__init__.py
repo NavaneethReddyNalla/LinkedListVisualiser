e98ac78e0b82310baa51e685ed80fd58f012c8f0
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-26 14:28:46 +0530

    Added a selection hitbox drawing

--- ui/screens/util/__init__.py
+++ ui/screens/util/__init__.py
@@ -1,2 +1,3 @@
 from .draw_ll import draw_node, un_draw_node
 from .animate import move_node_to, shift_list
+from .node_selection import Selector
--- ui/screens/util/node_selection.py
+++ ui/screens/util/node_selection.py
@@ -1,3 +1,24 @@
 from tkinter import Canvas
+from logic.LinkedList import LinkedList
 
 
+class Selector:
+    def __init__(self, canvas: Canvas, linked_list: LinkedList, deletion: bool = False):
+        self.canvas = canvas
+        self.linked_list = linked_list
+        self.boxes = []
+        self.deletion = deletion
+
+        self.draw()
+
+    def draw(self):
+        curr = self.linked_list.head
+
+        while curr:
+            coords = self.canvas.coords(curr.id)
+            coords = (coords[0] - 5, coords[1] - 5, coords[2] + 5, coords[3] + 5)
+
+            id = self.canvas.create_rectangle(*coords, outline="blue")
+            self.boxes.append(id)
+
+            curr = curr.next
