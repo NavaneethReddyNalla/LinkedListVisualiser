1d6a2dd455ca39f796741a12417a3d5003a8c5a2
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2023-12-26 14:42:48 +0530

    Made the Hitbox more responsive

--- ui/screens/util/node_selection.py
+++ ui/screens/util/node_selection.py
@@ -14,11 +14,13 @@ class Selector:
     def draw(self):
         curr = self.linked_list.head
 
+        fill = "green" if not self.deletion else "red"
+
         while curr:
             coords = self.canvas.coords(curr.id)
             coords = (coords[0] - 5, coords[1] - 5, coords[2] + 5, coords[3] + 5)
 
-            id = self.canvas.create_rectangle(*coords, outline="blue")
+            id = self.canvas.create_rectangle(*coords, outline="blue", activefill=fill, width=5)
             self.boxes.append(id)
 
             curr = curr.next
