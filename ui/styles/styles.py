75d3a0bd60e8cba0d793c8b069b039da1f5d72a9
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-01-14 15:58:53 +0530

    Added a current.json file to store the current style applied

--- ui/styles/styles.py
+++ ui/styles/styles.py
@@ -1,3 +1,15 @@
+import os
+import json
+
 from .presets import *
 
 PRIMARY_COLORS = {**DEFAULT}
+HEADERS = ("frame", "canvas", "node", "data", "bg-label", "btn_text", "btn_primary", "btn_success", "btn_danger")
+PATH = "./ui/styles/current.json"
+
+if os.path.isfile(PATH):
+    with open(PATH, "r") as current:
+        PRIMARY_COLORS = json.load(current)
+else:
+    with open(PATH, "w") as current:
+        json.dump(PRIMARY_COLORS, current)
