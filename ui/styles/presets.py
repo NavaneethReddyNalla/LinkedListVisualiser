6422cb975f70264007b131a1aa1641b4073b2963
CodeRanger <66413662+NavaneethReddyNalla@users.noreply.github.com>
2024-01-14 14:14:39 +0530

    Added a preset style file

---
+++ ui/styles/presets.py
@@ -0,0 +1,20 @@
+DEFAULT = {
+    # Frames
+    "frame": "#5438DC",
+
+    # Canvas
+    "canvas": "white",
+
+    # Nodes
+    "node": "#2B9EB3",
+    "data": "white",
+
+    # Labels
+    "bg-label": "#5438DC",
+
+    # Buttons
+    "btn_text": "white",
+    "btn_primary": "#121619",
+    "btn_success": "#44AF69",
+    "btn_danger": "#F8333C",
+}
--- ui/styles/styles.py
+++ ui/styles/styles.py
@@ -1,20 +1,3 @@
-PRIMARY_COLORS = {
-    # Frames
-    "frame": "#5438DC",
+from .presets import *
 
-    # Canvas
-    "canvas": "white",
-
-    # Nodes
-    "node": "#2B9EB3",
-    "data": "white",
-
-    # Labels
-    "bg-label": "#5438DC",
-
-    # Buttons
-    "btn_text": "white",
-    "btn_primary": "#121619",
-    "btn_success": "#44AF69",
-    "btn_danger": "#F8333C",
-}
+PRIMARY_COLORS = DEFAULT
